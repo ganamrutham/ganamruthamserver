b9b06e796a03511c8fda30eb2466c825e65a0af4
chaitanya gollapudi <glchaitanya94@gmail.com>
2025-07-05 19:13:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+
+const nodemailer = require("nodemailer");
+
+app.get('/sendemail', (req, res) => {  
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "srivenkateswaraganamrutham@gmail.com",
+    pass: "imdg pgwj eaet ehun",
+  },
+});
+
+const mailOptions = {
+  from: "srivenkateswaraganamrutham@gmail.com",
+  to: "srivenkateswaraganamrutham@gmail.com",
+  subject: "Test Email from Node.js",
+  text: "Hello, this is a test email sent rom Node.js using Nodemailer",
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    return console.log(`Error occured:`, error);
+  }
+  console.log(`Email sent successfully:`, info.response);
+  res.send('Hello World!')
+})
+
+})
