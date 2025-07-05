48d4722e469e8a7daca8a59f64fffaced9af1a39
chaitanya gollapudi <glchaitanya94@gmail.com>
2025-07-05 21:12:55 +0530

    email body json

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const mailOptions = {
   from: "srivenkateswaraganamrutham@gmail.com",
   to: "srivenkateswaraganamrutham@gmail.com",
   subject: "New Message",
-  text: "hi checking status message"
+  text: req.body.name +"\n"+req.body.email +"\n" +req.body.message
 };
 
 transporter.sendMail(mailOptions, (error, info) => {
