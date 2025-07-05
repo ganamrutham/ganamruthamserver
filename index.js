97665d01136277b23c5d07f2b4423718b1bbfaeb
chaitanya gollapudi <glchaitanya94@gmail.com>
2025-07-05 21:37:03 +0530

    email body json

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
-
-app.use(bodyParser)
-var dynamicCorsOptions = function(req, callback) {
-    var corsOptions;
-    if (req.path.startsWith('/sendemail')) {
-      corsOptions = {
-        origin: 'https://www.srivenkateswaraganamrutham.org/', // Allow only a specific origin
-        credentials: true,            // Enable cookies and credentials
-      };
-    } else {
-      corsOptions = { origin: '*' };   // Allow all origins for other routes
-    }
-    callback(null, corsOptions);
-  };
-  
-  app.use(cors(dynamicCorsOptions));
-//app.use(cors())
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
@@ -48,13 +29,12 @@ const mailOptions = {
   from: "srivenkateswaraganamrutham@gmail.com",
   to: "srivenkateswaraganamrutham@gmail.com",
   subject: "New Message",
-  html: req.body.name
+  text: "hi checking status message"
 };
 
 transporter.sendMail(mailOptions, (error, info) => {
   if (error) {
     return console.log(`Error occured:`, error);
-    //res.status(500).send(error)
   }
   console.log(`Email sent successfully:`, info.response);
   res.status(200).send('success')
