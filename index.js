bcddcbbbeae91b3db40fdc7d69592d5117d21058
chaitanya gollapudi <glchaitanya94@gmail.com>
2025-07-05 21:25:50 +0530

    email body json

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+app.use(bodyParser)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
